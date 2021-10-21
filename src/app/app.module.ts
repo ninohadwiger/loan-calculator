e8ab20a2e6430eb1133d93b77a483e276f4b2cf9
Nino Hadwiger <ninohadwiger@yahoo.de>
2021-10-21 18:14:26 +0200

    add primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
