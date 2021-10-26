4cff79a73c7f9936279df34c6c262bfdc85f155f
Nino Hadwiger <ninohadwiger@yahoo.de>
2021-10-26 20:50:21 +0200

    create backend proxy

--- src/app/loan/services/loan-calculator.service.ts
+++ src/app/loan/services/loan-calculator.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {environment} from "../../../environments/environment";
 import {CalculationPayload} from "../models/calculation-payload.model";
+import {environment} from "../../../environments/environment";
 
 @Injectable({providedIn: 'root'})
 export class LoanCalculatorService {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://homework.fdp.workers.dev/',
+  apiUrl: '/api',
   apiKey: 'swb-222222'
 };
 
